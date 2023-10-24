054720da4ea6b50af3040da77faff6bda4728c78
Bodrova2002 <71357142+Bodrova2002@users.noreply.github.com>
2023-10-24 20:57:39 +0300

    Revert "Task1"

--- utils/extract_prices.py
+++
@@ -1,2 +0,0 @@
-def extract_prices(products):
-    return list(map(lambda product: product.price, products))
--- utils/select_products_by_category.py
+++
@@ -1,3 +0,0 @@
-def select_products_by_category(products, category):
-    return list(filter(lambda product: product.category == category, products))
-
